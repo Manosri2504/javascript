ebd94c56b108de470c5b462d5892ccfccbf52e03
Mano <manosri250402@gmail.com>
2026-04-17 15:57:42 +0530

    message

--- task1.js
+++ task1.js
@@ -1,3 +1,84 @@
+//task1
 var a = 50
 a = 100
 console.log(a);
+
+
+//task2
+let marks = 80
+marks = 80+10
+console.log(marks);
+
+
+//task3
+const price = 500
+const finalprice = price + 100
+console.log(finalprice);
+
+
+//task4
+console.log("Welcome Team");
+console.log(2026);
+console.log(true);
+
+
+//task5
+let name = "Java script"
+console.log(typeof(name));
+
+let num = 250
+console.log(typeof(num));
+
+let boo = true
+console.log(typeof(boo));
+
+
+//task6
+let fruits = ["Apple","Orange","Mango","Papaya","Grapes"]
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[fruits.length -1]);
+
+
+//task7
+student = {
+ name: "Naveen",
+ age: 20,
+ course: "MERN"
+}
+console.log(student.name);
+console.log(student.age);
+
+
+//task8
+console.log(20 + 10);
+console.log(50 - 25);
+console.log(5 * 5);
+console.log(100 / 4);
+console.log(20 % 3);
+
+
+//task9
+let x = 5;
+x++;
+console.log(x);
+
+let y = 10;
+y--;
+console.log(y);
+
+//task10
+console.log(10 > 5);
+console.log(5 < 2);
+console.log(20 == "20");
+console.log(15 === "15");
+console.log(10 != 8);
+
+//task11
+console.log(5 > 2 && 10 > 3);
+console.log(7 < 5 || 8 > 2);
+console.log (!(10 > 5));
+
+//task12
+let age = 18;
+age >=18 ? console.log("Eligible"):console.log("Not Eligible");
